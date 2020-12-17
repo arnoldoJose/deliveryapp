8a99c54657c20771c912adc6b838a1f077e8811e
arnoldoJose <aolivasgonzales73@gmail.com>
2020-12-16 21:28:21 -0600

    style buttom

--- src/Components/ComponentOrders/ComponentFood/CardsFood.js
+++ src/Components/ComponentOrders/ComponentFood/CardsFood.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
-import '../../../css/Shadows.css';
+
 const CardsFood = ({data}) => {
   return (
     <div className="col-sm-5 col-md-4 col-lg-3 mx-auto m-3">
@@ -11,8 +11,8 @@ const CardsFood = ({data}) => {
           <p>{data.name}</p>
   <label htmlFor="" className="d-flex">Precio: C$<p>{data.precio}</p></label>
         </div>
-      <div className="card-footer">
-        <Link to={`/find-product/${data._id}`} className="btn btn-warning btn-act">Ver Producto</Link>
+      <div className="card-footer mx-auto">
+        <Link to={`/find-product/${data._id}`} style={{width: 12+"em"}} className="btn btn-warning">Ver Producto</Link>
       </div>
       </div>
     </div>
